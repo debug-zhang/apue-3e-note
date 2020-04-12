c6fe68f9347aa564cc9771ddf4bd0c3649fbc7c7
blackbird52 <xinzezhang@outlook.com>
2020-04-12 23:27:33 +0800

    add chapter 02

---
+++ src/lib/pathalloc.c
@@ -0,0 +1,73 @@
+#include "apue.h"
+#include <errno.h>
+#include <limits.h>
+
+#ifdef PATH_MAX
+static long pathmax = PATH_MAX;
+#else
+static long pathmax = 0;
+#endif
+
+static long posix_version = 0;
+static long xsi_version = 0;
+
+/* If PATH_MAX is indeterminate, no guarantee this is adequate */
+#define PATH_MAX_GUESS 1024
+
+char *
+path_alloc(size_t *sizep) /* also return allocated size, if nonnull */
+{
+	char	*ptr;
+	size_t	size;
+
+	if (posix_version == 0) {
+		posix_version = sysconf(_SC_VERSION);
+	}
+
+	if (xsi_version == 0) {
+		xsi_version = sysconf(_SC_XOPEN_VERSION);
+	}
+
+	if (pathmax == 0) {		/* first time through */
+		errno = 0;
+		/*
+			The value returned by pathconf is the maximum size of a relative pathname 
+			when the first argument is the working directory, 
+			so we specify the root as the first argument and add 1 to the result. 
+			If pathconf indicates that PATH_MAX is indeterminate, 
+			we have to punt and just guess a value.
+		*/
+		if ((pathmax = pathconf("/", _PC_PATH_MAX)) < 0) {
+			if (errno == 0) {
+				/* it's indeterminate */
+				pathmax = PATH_MAX_GUESS;
+			} else {
+				err_sys("pathconf error for _PC_PATH_MAX");
+			}
+		} else {
+			/* add one since it's relative to root */
+			pathmax++;
+		}
+	}
+
+	/*
+	   Before POSIX.1-2001, we aren't guaranteed that PATH_MAX includes
+	   the terminating null byte. Same goes for XPG3.
+	*/
+	if ((posix_version < 200112L) && (xsi_version < 4)) {
+		size = pathmax + 1;
+	} else {
+		size = pathmax;
+	}
+
+	if ((ptr = malloc(size)) == NULL) {
+		err_sys("malloc error for pathname");
+	}
+
+	if (sizep != NULL) {
+		*sizep = size;
+	}
+
+	return (ptr);
+}
+		
