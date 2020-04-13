2980fbe9be457c1289d75f588b4dd3ba6ed6b927
blackbird52 <xinzezhang@outlook.com>
2020-04-14 04:33:55 +0800

    add chapter 03

---
+++ src/fileio/setfl.c
@@ -0,0 +1,22 @@
+#include "apue.h"
+#include <fcntl.h>
+
+void
+set_fl(int fd, int flags)	/* flags are file status flags to turn on */
+{
+	int val;
+
+	if ((val = fcntl(fd, F_GETFL, 0)) < 0) {
+		err_sys("fcntl F_GETFL error");
+	}
+
+	/* turn on flags */
+	val |= flags;
+
+	/* turn flags off */
+	/* val &= ~flags; */
+
+	if (fcntl(fd, F_SETFL, val) < 0) {
+		err_sys("fcntl F_SETFL error");
+	}
+}
